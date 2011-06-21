f319ccf4da32bacbe9ff3fad0380386ddbd7717c
Pocket7878 <poketo7878@gmail.com>
2011-06-21 22:25:33 +0900

    first commit

--- cl-rc.asd
+++ cl-rc.asd
@@ -12,7 +12,6 @@
 ;;
 ;;You should have received a copy of the GNU General Public License
 ;;along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 (defsystem :cl-rc
 	   :version "1.0"
 	   :license "GPLv3"
--- package.lisp
+++ package.lisp
@@ -12,6 +12,7 @@
 ;;
 ;;You should have received a copy of the GNU General Public License
 ;;along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 (defpackage :cl-rc
   (:use :common-lisp :cl-ppcre :cl-interpol)
   (:export
